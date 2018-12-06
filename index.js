c7817ab92122a04aa97dd50cac63d0e915dd33a2
Trevar Williams <williams.trevar@gmail.com>
2018-12-05 20:39:02 -0800

    Left to do: error handling & top scroll

--- index.js
+++ index.js
@@ -3,18 +3,28 @@
 function searchSubmit() {
   $('.search-form').submit(event => {
     event.preventDefault();
+    // Clear out existing results if any
+    $('.js-search-header').html('');
+    $('.js-search-results').html('');
+    // Get search value
     const queryTarget = $(event.currentTarget).find('#band-search');
     const query = queryTarget.val();
+    // Get API info
     getArtistFromLastFM(query, displayArtistData);
     getSimilarFromLastFM(query, displaySimilarData);
-    queryTarget.val("");
+    // Clear out search field
+    queryTarget.val('');
+    //Hide landing page
+    $('.landing-page-container').prop('hidden', true);
+    // Unhide results container
+    $('.search-results').prop('hidden', false);
   });
 }
 
 const lastfm_search_url = 'https://ws.audioscrobbler.com/2.0/';
 // last.fm key: b555326f947cfc49eb798cc3643beaab
 
-// Finds information on the searched artist
+// Find information on the searched artist
 function getArtistFromLastFM(searchTerm, callback) {
   const query = {
     artist: `${searchTerm}`,
@@ -22,49 +32,56 @@ function getArtistFromLastFM(searchTerm, callback) {
     api_key: 'b555326f947cfc49eb798cc3643beaab',
     method: 'artist.getInfo',
     format: 'json',
-    }
+  }
   $.getJSON(lastfm_search_url, query, callback);
 }
 
-// Finds similar artist from searched artist
+// Find similar artist from searched artist
 function getSimilarFromLastFM(searchTerm, callback) {
   const query = {
     artist: `${searchTerm}`,
     autocorrect: 1,
-    limit: 10,
+    limit: 20,
     api_key: 'b555326f947cfc49eb798cc3643beaab',
     method: 'artist.getSimilar',
     format: 'json',
-    }
+  }
   $.getJSON(lastfm_search_url, query, callback);
 }
 
-// Generates & injects HTML for searched artist
+// Generate & inject HTML for searched artist
 function displayArtistData(data) {
-   const resultsHeader = `<div class="resultsHeader" style="background-image: url(${data.artist.image[4][`#text`]})">
-    <h2><a href="${data.artist.url}" target="_blank">${data.artist.name}</a></h2>
+  const resultsHeader = `
+    <a href="${data.artist.url}" target="_blank">
+      <div class="resultsHeader" style="background: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url(${data.artist.image[4][`#text`]}) no-repeat;">
+      <span class="band-name">${data.artist.name}</span>
+      </div>
+    </a>
+    <div class="artist-bio">
+    <span>${data.artist.bio.summary}</span>
     </div>
-    <div class="similarHeader">
-    <h2>Similar Artists</h2>
-    </div>`;
-  $('.search-results').prepend(resultsHeader);
+    `;
+  $('.js-search-header').append(resultsHeader);
+  console.log(data);
 }
 
+// Set HTML for similar artists
+function generateSimilarResults(result) {
+  return `
+    <a href="${result.url}" target="_blank">
+      <div class="similarResults" style="background: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url(${result.image[4][`#text`]}) no-repeat;">
+      <span class="band-name">${result.name}</span>
+      </div>
+    </a>`;
+}
+
+// Generate and inject similar artists results
 function displaySimilarData(data) {
   // Generates HTML body for similar artists from search
   const result = data.similarartists.artist.map((item, index) => generateSimilarResults(item));
   // Inject search results
-  $('.search-results')
-    .append(result)
-    .prop('hidden', false);
-}
-
-// Sets HTML for similar artists
-function generateSimilarResults(result) {
-  return `
-    <div class="results" style="background-image: url(${result.image[4][`#text`]})">
-    <a href="${result.url}" target="_blank">${result.name}</a><br>
-    </div>`;
+  $('.js-search-results').prepend(`<h2>Similar Artists</h2>`);
+  $('.js-search-results').append(result);
 }
 
 $(searchSubmit);
